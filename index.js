ed703e894105ac380374d81368531065e382486a
ayshabilaii <aysha.bilai@ontariotechu.net>
2024-11-04 12:20:40 -0500

    initial commit

--- index.js
+++ index.js
@@ -12,3 +12,27 @@ const server = http.createServer((req, res) => {
 server.listen(port, hostname, () => {
   console.log('Server running at http://${hostname}:${port}/');
 })
+
+// require module
+var connect = require('connect')
+ 
+// create app
+var app = connect()
+
+//lecture two video
+function helloworld(req,res, next)
+{
+  res.setHeader('Content-Type','text/plain');
+  res.end("Hello World");
+}
+
+function GoodByeworld(req,res, next)
+{
+  res.setHeader('Content-Type','text/plain');
+  res.end("Goodbye World");
+}
+
+app.use('/Aboutme',Aboutme);
+app.use('/Contactus',Contactus);
+app.listen(3000);
+console.log('Sever is running on local host')
